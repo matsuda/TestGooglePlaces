2dc3bf9f336c87fc6c5d581cc8aed9242d1baf19
matsuda <kosukematsuda@gmail.com>
2018-02-11 16:48:46 +0900

    create project

---
+++ TestGooglePlaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestGooglePlaces
+//
+//  Created by Kosuke Matsuda on 2018/02/11.
+//  Copyright © 2018年 matsuda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
