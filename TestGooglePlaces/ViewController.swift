52f974952ff246633e52486004f57edc925e6833
matsuda <kosukematsuda@gmail.com>
2018-02-12 16:43:59 +0900

    位置情報の取得

--- TestGooglePlaces/ViewController.swift
+++ TestGooglePlaces/ViewController.swift
@@ -16,9 +16,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
 
+    var locationManager: CLLocationManager!
+    var coordinate: CLLocationCoordinate2D?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.delegate = self
         placesClient = GMSPlacesClient.shared()
     }
 
@@ -47,12 +53,36 @@ class ViewController: UIViewController {
             self.addressLabel.text = ""
 
             if let placeLikelihoodList = placeLikelihoodList {
-                let place = placeLikelihoodList.likelihoods.first?.place
-                if let place = place {
-                    self.nameLabel.text = place.name
-                    self.addressLabel.text = place.formattedAddress?.components(separatedBy: ", ")
-                        .joined(separator: "\n")
+                for likelihood in placeLikelihoodList.likelihoods {
+                    let place = likelihood.place
+                    print("--------------------------------------")
+                    print("Current Place name \(place.name) at likelihood \(likelihood.likelihood)")
+                    print("Current Place address \(place.formattedAddress)")
+                    print("Current Place attributions \(place.attributions)")
+                    print("Current PlaceID \(place.placeID)")
+                    print("Current Place coordinate >>>", place.coordinate)
                 }
+            }
+        }
+    }
+
+    func requestCurrentPlace() {
+        guard let coordinate = coordinate else {
+            return
+        }
+        let location = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { (placemarks, error) in
+            if let error = error {
+                print("error >>>", error)
+                return
+            }
+            guard let placemarks = placemarks, placemarks.count > 0 else {
+                print("empty placemarks")
+                return
+            }
+            for placemark in placemarks {
+                print("placemark >>>", placemark)
             }
         }
     }
@@ -75,5 +105,43 @@ extension ViewController: GMSAutocompleteViewControllerDelegate {
     func wasCancelled(_ viewController: GMSAutocompleteViewController) {
         print("Autocomplete was cancelled.")
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension ViewController: CLLocationManagerDelegate {
+    func startUpdateLocation(manager: CLLocationManager) {
+        coordinate = nil
+//        manager.startUpdatingLocation()
+        manager.requestLocation()
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        print(#function)
+        print("status >>>", status.rawValue)
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+            break
+        case .denied:
+            break
+        case .restricted:
+            break
+        case .authorizedWhenInUse, .authorizedAlways:
+            startUpdateLocation(manager: manager)
+            break
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(#function)
+        guard let location = locations.last else {
+            return
+        }
+        print("location >>>", location)
+        coordinate = location.coordinate
+        requestCurrentPlace()
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(#function)
+        print("error >>>", error)
+        locationManager.stopUpdatingLocation()
     }
 }
